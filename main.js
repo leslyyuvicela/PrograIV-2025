0083696b7a96df9318cbe975e300abe5f32cccf0
leslyyuvicela <yuvicelaguzman14727@gmail.com>
2025-02-26 23:54:32 -0600

    Lab 1

--- main.js
+++ main.js
@@ -1,41 +1,97 @@
-const {createApp, ref} = Vue;
-const Dexie = window.Dexie,
-    db = new Dexie('db_usss037423_usss026723');
+const {createApp} = Vue;
 
-const app = createApp({
-    components: {
-        autor,
-        libro,
-        buscarautor,
-        buscarlibro
-    },
+createApp({
     data() {
         return {
-            forms : {
-                autor: {mostrar: false},
-                buscarAutor: {mostrar: false},
-                libro: {mostrar: false},
-                buscarLibro: {mostrar: false},
-                matricula: {mostrar: false},
-            },
-        };
+            alumnos: [],
+            alumnosMostrados: [],
+            campoFiltrado: 'nombre',
+            filtro: '',
+            codigo: '',
+            nombre: '',
+            direccion: '',
+            telefono: '',
+            email: '',
+            nacimiento: '',
+            sexo: '',
+            departamento: '',
+            municipio: '',
+            distrito: '',
+        }
     },
     methods: {
-        buscar(form, metodo) {
-            this.$refs[form][metodo]();
+        eliminarAlumno(alumno) {
+            if (confirm(`¿Esta seguro de eliminar el alumno ${alumno.nombre}?`)){
+                localStorage.removeItem(alumno.codigo);
+                this.listarAlumnos();
+                //limpiar campos
+                this.codigo = '';
+                this.nombre = '';
+                this.direccion = '';
+                this.telefono = '';
+                this.email = '';
+                this.nacimiento = '';
+                this.sexo = '';
+                this.departamento = '';
+                this.municipio = '';
+                this.distrito = '';
+            }
         },
-        abrirFormulario(componente) {
-            this.forms[componente].mostrar = !this.forms[componente].mostrar;
+        verAlumno(alumno) {
+            this.codigo = alumno.codigo;
+            this.nombre = alumno.nombre;
+            this.direccion = alumno.direccion;
+            this.telefono = alumno.telefono;
+            this.email = alumno.email;
+            this.nacimiento = alumno.nacimiento;
+            this.sexo = alumno.sexo;
+            this.departamento = alumno.departamento;
+            this.municipio = alumno.municipio;
+            this.distrito = alumno.distrito;
         },
-        modificar(form, metodo, datos) {
-            this.$refs[form][metodo](datos);
+        guardarAlumno() {
+            let alumno = {
+                codigo: this.codigo,
+                nombre: this.nombre,
+                direccion: this.direccion,
+                telefono: this.telefono,
+                email: this.email,
+                nacimiento: this.nacimiento,
+                sexo: this.sexo,
+                departamento: this.departamento,
+                municipio: this.municipio,
+                distrito: this.distrito,
+            };
+            localStorage.setItem(this.codigo, JSON.stringify(alumno));
+            this.listarAlumnos();
+            //limpiar campos
+            this.codigo = '';
+            this.nombre = '';
+            this.direccion = '';
+            this.telefono = '';
+            this.email = '';
+            this.nacimiento = '';
+            this.sexo = '';
+            this.departamento = '';
+            this.municipio = '';
+            this.distrito = '';
+        },
+        listarAlumnos() {
+            this.alumnos = [];
+            for (let i = 0; i < localStorage.length; i++) {
+                let clave = localStorage.key(i),
+                    valor = localStorage.getItem(clave);
+                this.alumnos.push(JSON.parse(valor));
+            }
+            this.alumnosMostrados = this.alumnos;
+        },
+        filtrarAlumnos(){
+            this.alumnosMostrados = this.alumnos.filter(alumno => {
+                return alumno[this.campoFiltrado].toLowerCase().includes(this.filtro.toLowerCase());
+            });
         }
     },
     created() {
-        db.version(1).stores({
-            autores: '++idAutor, codigo, nombre, pais, telefono',
-            libros: '++idLibro, idAutor, isbn, titulo, editorial, edicion',
-        });
+        this.listarAlumnos();
     }
-});
-app.mount('#app');
+}).mount('#app');
